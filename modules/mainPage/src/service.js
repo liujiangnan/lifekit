705577f43032b90254755064b745fa00d7f42625
liujiangnan <695541723@qq.com>
2016-11-09 01:41:54 +0800

    首页模块开发

---
+++ modules/mainPage/src/service.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhy on 16/11/3.
+ */
+
+function service(){
+
+
+
+}
+module.exports = service;
---
+++ modules/talkhome/src/service.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zhy on 16/11/8.
+ */
+function  service(){
+
+}
+
+module.exports = service;
