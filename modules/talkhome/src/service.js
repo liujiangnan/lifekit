af1ffe22362352fc77bcb2c94cdb0c29627673d3
liujiangnan <695541723@qq.com>
2016-11-13 16:22:23 +0800

    聊天实现

--- modules/talkhome/src/service.js
+++ modules/talkhome/src/service.js
@@ -1,8 +1,20 @@
 /**
  * Created by zhy on 16/11/8.
  */
+
 function  service(){
 
+    this.pushMessage = function(req,res,parms){
+
+        var clients = net.getSockets("modules/talkhome/src/service");
+        var arr = clients.values();
+        for(var i=0;i<arr.length;i++){
+            var obj = arr[i];
+            var sc = obj.socket;
+            sc.emit("getMessage",parms);
+        }
+        res.send('success');
+    }
 }
 
 module.exports = service;
--- modules/talkhome/web/index.js
+++ modules/talkhome/web/index.js
@@ -1,4 +1,25 @@
+
+net.on("getMessage",function(data){
+    var dom = $("#messageShow").clone();
+    dom.find(".popover-content").html(data);
+    $('#talkView').append(dom);
+    dom.show();
+    $('#talkView').scrollTop( $('#talkView')[0].scrollHeight );
+});
+
+
 $(function () {
 
+    $('#sendMessage').bind('click',function(){
+        var message = $("#message").val();
+        message = message==""?$("#message").text():message;
+        if(message){
+            net.getData("pushMessage",message,function(msg){
+                if(msg==='success'){
+                    $("#message").val('');
+                }
+            });
+        }
+    });
 
 });
