6430fbbae384f440371b44dd116d7f56298c2b5d
liujiangnan <695541723@qq.com>
2018-03-30 00:55:08 +0800

    禁用状态不好使

--- engine/lk-imu/src/service.js
+++ engine/lk-imu/src/service.js
@@ -3,14 +3,17 @@ function service(net){
 
   this.runStatus = async function(ctx,parms){
     net.data.form = {
-      name: 'abc',
-      cpu: '111',
-      date1: '',
-      date2: '',
-      delivery: false,
-      type: [],
-      resource: '',
-      desc: ''
+      firstDate: Date.now(),  //首次开机时间
+      thisDate: '', //本次开机时间
+      totleTime: '', //累计运行时长
+      thisTime: '', //本次运行时长
+      cpu: '95%', //CPU利用率
+      ram: '', //内存利用率
+      rom: '', //硬盘利用率
+
+      url: '/lk-imu',  //web域名
+      effectiveDate: Date.now()+30*24*60*60*1000,  //有效日期
+      isEdit: false  //是否可编辑
     }
     return ctx.render("lk-imu/web/devinfo/run_status.ejs",{});
   }
