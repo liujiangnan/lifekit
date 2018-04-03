39e40816d85bb087e06d85ceebaa85e0f794c21b
liujiangnan <695541723@qq.com>
2018-04-04 00:14:44 +0800

    功能修改

--- engine/lk-imu/src/serial.js
+++ engine/lk-imu/src/serial.js
@@ -9,17 +9,35 @@ function serial (net){
   //树属性
   net.data.treeProps = { 
     id: "id",
-    label: "label",
+    label: "name",
     children: "children"
   };
   //串口属性
   net.data.serialProp = {
-    cpu:""
+    id:"",
+    name:"",
+    discription:"",
+    status:"",  //串口状态
+    sendbit:"",  //发送字节
+    receivebit:"",  //接收字节
+    errorRate:"",  //误码率
+    baudRate:"",  //波特率
+    bitLength:"",  //数据字长
+    validate:"",  //校验方式
+    stopBit:"", //停止位
+
+    drive:"",  //驱动
+    pamam:"",  //参数 
+    commandTimeInterval:"", //命令时间间隔
+    bytesStreamMode:""  //字节流方式 
   }
   //设备列表
   net.data.devList = [{name:"dev1",status:'1',address:'1'}];
   this.init = async function(ctx,parms){ 
     return ctx.render("lk-imu/web/serial/index.ejs",{});
+  }
+  this.saveSerial = function(){
+
   }
 }
 
